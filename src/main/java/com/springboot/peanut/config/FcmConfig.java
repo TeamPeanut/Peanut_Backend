e51e5549681eece309aa37f44ad3749a7152d8b0
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2024-11-06 12:33:50 +0900

    FCM설정수정

--- src/main/java/com/springboot/peanut/config/FcmConfig.java
+++ src/main/java/com/springboot/peanut/config/FcmConfig.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Slf4j
@@ -20,9 +20,9 @@ public class FcmConfig {
 
     @Bean
     public void initializeFirebaseApp() throws IOException {
-        // FileInputStream을 사용하여 파일 시스템에서 직접 Firebase JSON 파일 로드
+        // ClassPathResource를 사용하여 리소스 폴더 내 파일 접근
         GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new FileInputStream(firebaseConfigPath));
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(googleCredentials)
